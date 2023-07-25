4d341bc019b7d1b1c1163ecb100f5913ad64c0e8
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2023-07-25 19:17:25 +0530

    layout modification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-
+import ThemeProvider from '@/contexts/ThemeProvider';
+import Darkmode from './Darkmode';
+import SideBar from '@/components/SideBar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,10 +13,27 @@ export const metadata: Metadata = {
     'a twitter clone created using React, NextJs, tailwind, superbase',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider>
+          <Darkmode>
+            <div className="w-screen h-screen text-black  bg-white dark:bg-black dark:text-whitetextcolor overflow-x-hidden relative">
+              <div className="max-w-[1250px] w-full mx-auto flex relative">
+                <section className="h-screen sticky top-0 laptop:w-2/12 max-tablet:w-1/12 max-mobile:w-1/6">
+                  <SideBar />
+                </section>
+                {children}
+              </div>
+            </div>
+          </Darkmode>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,15 @@
-import SideBar from '@/components/SideBar';
 import Maincontent from '@/components/Maincontent';
 import Rightbar from '@/components/Rightbar';
-import ThemeProvider from '@/contexts/ThemeProvider';
-import Darkmode from './Darkmode';
 
 export default function Home() {
   return (
-    <ThemeProvider>
-      <Darkmode>
-        <div className="w-screen h-screen text-black  bg-white dark:bg-black dark:text-whitetextcolor overflow-x-hidden relative">
-          <div className="max-w-[1250px] w-full mx-auto flex relative">
-            <section className="h-screen sticky top-0 laptop:w-2/12 max-tablet:w-1/12 max-mobile:w-1/6">
-              <SideBar />
-            </section>
-            <main className="laptop:w-7/12 max-tablet:w-8/12 max-mobile:w-5/6">
-              <Maincontent />
-            </main>
-            <section className="h-screen sticky top-0 laptop:w-2/12 max-mobile:hidden">
-              <Rightbar />
-            </section>
-          </div>
-        </div>
-      </Darkmode>
-    </ThemeProvider>
+    <>
+      <main className="laptop:w-7/12 max-tablet:w-8/12 max-mobile:w-5/6">
+        <Maincontent />
+      </main>
+      <section className="h-screen sticky top-0 laptop:w-2/12 max-mobile:hidden">
+        <Rightbar />
+      </section>
+    </>
   );
 }
